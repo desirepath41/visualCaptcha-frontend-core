391eeaec1d6abc77de438e5d47d249f66e5fa655
Alexander Bykhov <another.side.of.my.soul@gmail.com>
2014-02-10 20:08:46 +0400

    update angular directive to use $http for requests

--- src/visualcaptcha.angular.js
+++ src/visualcaptcha.angular.js
@@ -2,21 +2,32 @@
 
 define( [
     'angular',
-    'visualcaptcha.vanilla'
-], function( angular, visualCaptcha ) {
+    'visualcaptcha.vanilla',
+    'visualcaptcha/deep-extend'
+], function( angular, visualCaptcha, deepExtend ) {
     'use strict';
 
     angular
         .module( 'visualCaptcha', [] )
-        .directive( 'captcha', function() {
+        .directive( 'captcha', [ '$http', function( $http ) {
+            var _request = function( url, callback ) {
+                $http({
+                    method: 'GET',
+                    url: url
+                }).success( callback );
+            };
             return {
                 restrict: 'A',
                 scope: {
                     'options': '='
                 },
                 link: function( scope, element ) {
-                    visualCaptcha( element[ 0 ], scope.options );
+                    visualCaptcha( element[ 0 ], deepExtend( scope.options, {
+                        captcha: {
+                            request: _request
+                        }
+                    } ) );
                 }
             };
-        } );
+        }] );
 } );
